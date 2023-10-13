507e655823d608484890fb66c491e43c6eb727cc
bx5974 <bx5974@gmail.com>
2023-10-13 16:47:27 +0530

    Create main.tf
    init import

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "random_string" "datagen" {
+	length = 30
+}
+
+resource "random_integer" "idnum" {
+	min = 10
+	max = 200
+}
+
+variable "sample" {
+	description = "hello folks "
+}
+
+output "randint" {
+	value = "${var.sample}-${random_integer.idnum.result}"
+}
+
+output "myrandstring" {
+	value = random_string.datagen.result
+}
